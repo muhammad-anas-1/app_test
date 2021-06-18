b46dafa154bdfe28657c6d3c8865d5c75b3fc819
My app-test <itsranas517@gmail.com>
2021-06-18 05:19:36 -0700

    Add edit, show, update and index in listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,19 @@
 class ArticlesController < ApplicationController
-
+  def index
+    @articles = Article.all
+  end
   def new
 
     @article = Article.new
 
+  end
+  def edit
+    @article= Article.find(params[:id])
+  end
+  def update
+    @article= Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
   def create
     # render plain: params[:article].inspect
@@ -22,5 +32,6 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :description)
 
   end
+
 
 end
